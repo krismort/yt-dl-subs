8bfe52b7ffa714093c2c01d4f570d1e7101d06da
krismort <email@hidden.com>
2024-04-23 17:05:19 +0300

    added argument to print timestamps

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const { getSubtitles } = require('youtube-captions-scraper');
 const arg = process.argv[2] || process.env.ID || null;
 let videoID = arg;
 
+const arg2 = process.argv[3] ?? null;
+
+function getParameterByName(url, paramName) {
+  const parsedUrl = new URL(url);
+  return parsedUrl.searchParams.get(paramName);
+}
+
 // If the argument is a URL, extract the video ID
 if (videoID && videoID.includes('youtube.com') || videoID.includes('youtu.be')) {
-    const urlPattern = /(?:v=|\/)([^&\n?]+)/;
-    const match = videoID.match(urlPattern);
-    if (match && match[1]) {
-        videoID = match[1];
-    }
+  const urlPattern = /(?:v=|\/)([^&\n?]+)/;
+  const match = videoID.match(urlPattern);
+  if (match && match[1]) {
+    videoID = getParameterByName(videoID, 'v');
+  }
 }
 
 if (!videoID) {
@@ -23,10 +30,12 @@ getSubtitles({
   videoID: videoID, // YouTube video ID
   lang: 'en' // Language of subtitles (use 'en' for English)
 }).then(subtitles => {
-    const text = subtitles.reduce((acc, iter, index) => {
-        return acc + `#${index} ${iter.start}: ${iter.text}\n`;
-    }, '');
-    console.log(text);
+  const text = subtitles.reduce((acc, iter, index) => {
+    return acc +
+      (arg2 === '-ts' ? `#${index} ${iter.start}: ` : '') +
+      `${iter.text}\n`;
+  }, '');
+  console.log(text);
 }).catch(error => {
   console.error('Error fetching subtitles:', error);
 });
